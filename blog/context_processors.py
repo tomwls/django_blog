365c2020aafdd9a12bbbc5269ba5cbdd806ad73e
Tom Wells <tomwells285@gmail.com>
2025-09-05 21:57:22 +0100

    Style

---
+++ blog/context_processors.py
@@ -0,0 +1,7 @@
+from .icons import CLOCK_ICON, ENTER_ICON
+
+def global_icons(request):
+    return {
+        'clock_icon': CLOCK_ICON,
+        'enter_icon': ENTER_ICON
+    }
---
+++ blog/icons.py
@@ -0,0 +1,5 @@
+CLOCK_ICON = '''
+<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+'''
+
+ENTER_ICON = '''<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path><polyline points="10 17 15 12 10 7"></polyline><line x1="15" y1="12" x2="3" y2="12"></line></svg>'''
