3492f1547a3c4e53ad4d8a790cb2f02b2d006b28
Tom Wells <tomwells285@gmail.com>
2025-09-06 10:51:18 +0100

    Search

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.order_by('-created_at') # order posts by newest first
+    query = request.GET.get('q')
+    if query:
+        posts = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query)).order_by('-created_at')
+    else:
+        posts = Post.objects.order_by('-created_at') # order posts by newest first
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, slug):
